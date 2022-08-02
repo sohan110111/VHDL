5945fd20bc525ca09b7a7ea732fe72cb72cae81e
Binary01 <binary01.dev@gmail.com>
2022-08-03 00:21:20 +0600

    First Program compile sumulation and run

---
+++ HelloWorld.vhd
@@ -0,0 +1,13 @@
+entity HelloWorld is
+end entity;
+
+architecture sim of HelloWorld is
+begin 
+
+    process is
+    begin
+        report "Hello World";
+        wait;
+    end process;
+
+end architecture;            
